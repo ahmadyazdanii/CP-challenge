486da982c650ee826b37a5163f9bd3a57d41244c
ahmadyazdanii <ahmad.yazdani.pub@gmail.com>
2024-04-23 13:32:10 +0330

    new [authentication]: write authentication service unit tests

---
+++ authentication-service/src/test/kotlin/com/chargepoint/authenticationservice/AuthenticationServiceTest.kt
@@ -0,0 +1,89 @@
+package com.chargepoint.authenticationservice
+
+import com.chargepoint.authenticationservice.common.AuthenticationResponseEvent
+import com.chargepoint.authenticationservice.common.AuthorizationStatus
+import com.chargepoint.authenticationservice.model.User
+import com.chargepoint.authenticationservice.repository.UsersRepository
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.*
+import org.springframework.kafka.core.KafkaTemplate
+
+class AuthenticationServiceTest {
+    private val kafkaTemplate = mockk<KafkaTemplate<String, AuthenticationResponseEvent>>(relaxed = true)
+    private val usersRepository = mockk<UsersRepository>()
+
+    private lateinit var authenticationService: AuthenticationService
+
+    @BeforeEach
+    fun setup() {
+        authenticationService = AuthenticationService(kafkaTemplate, usersRepository)
+    }
+
+    @Nested
+    @DisplayName("When getUserAuthorizationStatus was called")
+    inner class WhenGetUserAuthorizationStatusWasCalled {
+        @Test
+        fun `Should return Invalid status when userId not within valid length`() {
+            val shortUserId = "1234123412341234123" // 19 chars
+            val longUserId = "123412341234123412341234123412341234123412341234123412341234123412341234123412341" // 81 chars
+
+            assertAll(
+                {
+                    assertEquals(
+                        AuthorizationStatus.Invalid,
+                        authenticationService.getUserAuthorizationStatus(shortUserId)
+                    )
+                }, {
+                    assertEquals(
+                        AuthorizationStatus.Invalid,
+                        authenticationService.getUserAuthorizationStatus(longUserId)
+                    )
+                }
+            )
+        }
+        
+        @Test
+        fun `Should return Unknown status when userId does not exist`() {
+            val validUserId = "12345123451234512345" // 20 chars
+            every { usersRepository.findOneById(validUserId) } returns null
+
+            assertEquals(AuthorizationStatus.Unknown, authenticationService.getUserAuthorizationStatus(validUserId))
+        }
+
+        @Test
+        fun `Should return Rejected status when userId is not allowed to charge`() {
+            val validUserId = "12345123451234512345" // 20 chars
+            val user = User(validUserId, false)
+            every { usersRepository.findOneById(validUserId) } returns user
+
+            assertEquals(AuthorizationStatus.Rejected, authenticationService.getUserAuthorizationStatus(validUserId))
+        }
+        
+        @Test
+        fun `Should return Accepted status when all conditions are met`() {
+            val validUserId = "12345123451234512345" // 20 chars
+            val user = User(validUserId, true)
+            every { usersRepository.findOneById(validUserId) } returns user
+
+            assertEquals(AuthorizationStatus.Accepted, authenticationService.getUserAuthorizationStatus(validUserId))
+        }
+    }
+    
+    @Nested
+    @DisplayName("When produceAuthenticationResponseEvent was called")
+    inner class WhenProduceAuthenticationResponseEventWasCalled() {
+        @Test
+        fun `should send a message with correct parameters`() {
+            val requestId = "sample-request-id"
+            val status = AuthorizationStatus.Accepted
+            val event = AuthenticationResponseEvent(requestId, status)
+
+            authenticationService.produceAuthenticationResponseEvent(requestId, status)
+
+            verify { kafkaTemplate.send("AuthenticationResponses", event) }
+        }
+    }
+}
